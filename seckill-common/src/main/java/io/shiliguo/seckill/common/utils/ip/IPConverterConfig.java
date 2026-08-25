7379078c226272d50439d0db126347f7e85ca583
ShiLiguo123 <2669380883@qq.com>
2026-08-25 11:42:38 +0800

    refactor: 调整项目包结构并更新项目信息

---
+++ seckill-common/src/main/java/io/shiliguo/seckill/common/utils/ip/IPConverterConfig.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.shiliguo.seckill.common.utils.ip;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @version 1.0.0
+ * @description IPConverterConfig
+ */
+public class IPConverterConfig extends ClassicConverter {
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ seckill-order/seckill-order-application/src/main/java/io/shiliguo/seckill/order/application/event/handler/SeckillOrderRocketMQEventHandler.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.shiliguo.seckill.order.application.event.handler;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.shiliguo.seckill.common.constants.SeckillConstants;
+import io.shiliguo.seckill.order.domain.event.SeckillOrderEvent;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+/**
+ * @version 1.0.0
+ * @description 基于RocketMQ的订单事件处理器
+ */
+@Component
+@ConditionalOnProperty(name = "message.mq.type", havingValue = "rocketmq")
+@RocketMQMessageListener(consumerGroup = SeckillConstants.EVENT_ORDER_CONSUMER_GROUP, topic = SeckillConstants.TOPIC_EVENT_ROCKETMQ_ORDER)
+public class SeckillOrderRocketMQEventHandler implements RocketMQListener<String> {
+    private final Logger logger = LoggerFactory.getLogger(SeckillOrderRocketMQEventHandler.class);
+
+    @Override
+    public void onMessage(String message) {
+        if (StrUtil.isEmpty(message)){
+            logger.info("rocketmq|orderEvent|接收订单事件为空");
+            return;
+        }
+        SeckillOrderEvent seckillOrderEvent = this.getEventMessage(message);
+        if (seckillOrderEvent.getId() == null){
+            logger.info("rocketmq|orderEvent|订单参数错误");
+        }
+        logger.info("rocketmq|orderEvent|接收订单事件|{}", JSON.toJSON(seckillOrderEvent));
+    }
+
+    private SeckillOrderEvent getEventMessage(String msg){
+        JSONObject jsonObject = JSONObject.parseObject(msg);
+        String eventStr = jsonObject.getString(SeckillConstants.MSG_KEY);
+        return JSONObject.parseObject(eventStr, SeckillOrderEvent.class);
+    }
+}
