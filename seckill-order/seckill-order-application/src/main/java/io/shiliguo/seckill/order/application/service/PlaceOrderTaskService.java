4cbf95c136bbeac5229b5383b69c10a46e83ff04
ShiLiguo123 <2669380883@qq.com>
2026-08-25 11:14:15 +0800

    refactor: 调整项目包结构并更新项目信息

---
+++ seckill-order/seckill-order-application/src/main/java/io/shiliguo/seckill/order/application/service/PlaceOrderTaskService.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.shiliguo.seckill.order.application.service;
+
+import io.shiliguo.seckill.order.application.model.result.OrderTaskSubmitResult;
+import io.shiliguo.seckill.order.application.model.task.SeckillOrderTask;
+
+/**
+ * @version 1.0.0
+ * @description 订单任务服务
+ */
+public interface PlaceOrderTaskService {
+
+    /**
+     * 提交订单任务
+     */
+    boolean submitOrderTask(SeckillOrderTask seckillOrderTask);
+
+}
