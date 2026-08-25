60b9e8b5afb9b2ddebb97fb55913e878d30d6a94
ShiLiguo123 <2669380883@qq.com>
2026-08-25 11:26:24 +0800

    refactor: 调整项目包结构并更新项目信息

---
+++ seckill-common/src/main/java/io/shiliguo/seckill/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.shiliguo.seckill.common.exception;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException;
+import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
+import io.shiliguo.seckill.common.response.ResponseMessage;
+import io.shiliguo.seckill.common.response.ResponseMessageBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.UndeclaredThrowableException;
+
+/**
+ * @version 1.0.0
+ * @description 全局统一异常处理器
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    /**
+     * 全局异常处理，统一返回状态码
+     */
+    @ExceptionHandler(SeckillException.class)
+    public ResponseMessage<String> handleSeckillException(SeckillException e) {
+        logger.error("服务器抛出了异常：{}", e);
+        return ResponseMessageBuilder.build(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * Sentinel规则
+     */
+    @ExceptionHandler(UndeclaredThrowableException.class)
+    public ResponseMessage<String> handleUndeclaredThrowableException(UndeclaredThrowableException e) {
+        ErrorCode errorCode = null;
+        if (e.getUndeclaredThrowable() instanceof FlowException){
+            errorCode = ErrorCode.SENTINEL_FLOW;
+        }else if (e.getUndeclaredThrowable() instanceof DegradeException){
+            errorCode = ErrorCode.SENTINEL_DEGRADE;
+        }else if (e.getUndeclaredThrowable() instanceof ParamFlowException){
+            errorCode = ErrorCode.SENTINEL_PARAMAS;
+        }else if (e.getUndeclaredThrowable() instanceof SystemBlockException){
+            errorCode = ErrorCode.SENTINEL_SYSTEM;
+        }else if (e.getUndeclaredThrowable() instanceof AuthorityException){
+            errorCode = ErrorCode.SENTINEL_AUTHORITY;
+        }else {
+            errorCode = ErrorCode.SENTINEL_FLOW;
+        }
+        return ResponseMessageBuilder.build(errorCode.getCode(), errorCode.getMesaage());
+    }
+
+    /**
+     * 全局异常处理，统一返回状态码
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseMessage<String> handleException(Exception e) {
+        logger.error("服务器抛出了异常：{}", e);
+        return ResponseMessageBuilder.build(ErrorCode.SERVER_EXCEPTION.getCode(), e.getMessage());
+    }
+
+
+}
---
+++ seckill-order/seckill-order-domain/src/main/java/io/shiliguo/seckill/order/domain/service/SeckillOrderDomainService.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.shiliguo.seckill.order.domain.service;
+
+
+import io.shiliguo.seckill.order.domain.model.entity.SeckillOrder;
+
+import java.util.List;
+
+/**
+ * @version 1.0.0
+ * @description 订单领域层接口
+ */
+public interface SeckillOrderDomainService {
+
+    /**
+     * 保存订单
+     */
+    boolean saveSeckillOrder(SeckillOrder seckillOrder);
+
+    /**
+     * 根据用户id获取订单列表
+     */
+    List<SeckillOrder> getSeckillOrderByUserId(Long userId);
+
+    /**
+     * 根据用户id获取订单列表
+     */
+    List<SeckillOrder> getSeckillOrderByGoodsId(Long goodsId);
+
+    /**
+     * 以userId作为分片键删除订单
+     */
+    void deleteOrderShardingUserId(Long orderId, Long userId);
+
+    /**
+     * 以goodsId作为分片键删除订单
+     */
+    void deleteOrderShardingGoodsId(Long orderId, Long goodsId);
+}
